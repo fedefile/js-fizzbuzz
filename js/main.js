bf2581ee6c054bff6ca119865511ebb2dd751f33
federico filella <federico.filella@gmail.com>
2020-02-23 21:06:11 +0100

    .1

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 //  ma per i multipli di 3 stampi “Fizz” al posto del numero
 //  e per i multipli di 5 stampi Buzz.
 //  Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+var numeri = [i];
+for (var i = 1; i <= 100; i++) {
+    if (!(i % 3) && !(i % 5)) {
+        console.log("FizzBuzz");
+    } else if (!(i % 3)) {
+        console.log("Fizz");
+    } else if (!(i % 5)) {
+        console.log("Buzz");
+    } else {
+        console.log([i]);
+    }
+}
